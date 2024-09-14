4913c2a393a9af4f41fcfc9b1ef2549d59852d21
anusharebala <anusharebala@gmail.com>
2024-09-14 13:59:10 +0530

    code added

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -9,5 +9,14 @@ public class Sample {
 	public void appium() {
 		System.out.println("appium");
 	}
+	public void selenium1() {
+		System.out.println("selenium");
+	}
+	public void framework1() {
+		System.out.println("framework");
+	}
+	public void appium1() {
+		System.out.println("appium");
+	}
 	
 }
