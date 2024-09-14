beccecc863ecf816878622e34e29f859f85e6d0c
anusharebala <anusharebala@gmail.com>
2024-09-14 13:51:38 +0530

    started

---
+++ src/test/java/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+	public void selenium() {
+		System.out.println("selenium");
+	}
+	public void framework() {
+		System.out.println("framework");
+	}
+	public void appium() {
+		System.out.println("appium");
+	}
+	
+}
